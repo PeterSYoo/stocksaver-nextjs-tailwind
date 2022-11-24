1f2b0bb2d276431f7a9fb788a7aa5b706268b8e0
Peter Yoo <trustintofu@gmail.com>
2022-11-23 21:55:19 -0800

    feat: Api endpoint for user signup.

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,39 @@
+import usersConnect from '../../../database/usersConnect';
+import Users from '../../../models/Users';
+import { hash } from 'bcrypt';
+
+export default async function handler(req: any, res: any) {
+  usersConnect().catch((error) => res.json({ error: 'Connection Failed...!' }));
+
+  // only post method is accepted
+  if (req.method === 'POST') {
+    if (!req.body)
+      return res
+        .status(404)
+        .json({ error: `Don't have form DataTransfer...!` });
+    const { username, email, password } = req.body;
+
+    // check duplicate users
+    const checkexisiting = await Users.findOne({ email });
+    if (checkexisiting)
+      return res.status(422).json({ message: 'User Already Exists...!' });
+
+    // hash password
+    Users.create(
+      {
+        username,
+        email,
+        password: await hash(password, 12),
+      },
+      function (err: any, data: any) {
+        if (err) return res.status(404).json({ err });
+        res.status(201).json({ status: true, user: data });
+      }
+    );
+  } else {
+    // error status 500
+    res
+      .status(500)
+      .json({ message: 'HTTP method not valid only POST Accepted' });
+  }
+}
