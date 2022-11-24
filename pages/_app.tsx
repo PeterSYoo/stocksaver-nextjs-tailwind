8b33c0c719a1499f7e96134c7346e575e7ba0c1e
Peter Yoo <trustintofu@gmail.com>
2022-11-23 19:21:01 -0800

    feat: Wraps app with theme provider.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Stocksaver - Save your tickers!</title>
+      </Head>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
