0a4d06b09772f1a6f50e5c7fe5f569c371b8101e
Peter Yoo <trustintofu@gmail.com>
2022-11-24 20:42:32 -0800

    feat: Returns null on initial render, useEffect to set hydrated state and render page if hydrated is true.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header.components';
+import { Signup } from '../components/Signup.components';
 
 const Home = () => {
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
   return (
     <>
-      <Header />
+      {hydrated ? (
+        <>
+          <Header />
+          <Signup />
+        </>
+      ) : null}
     </>
   );
 };
