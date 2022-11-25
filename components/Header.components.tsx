e875bfd3e71d56da7de4d9495487e2a32a885cd8
Peter Yoo <trustintofu@gmail.com>
2022-11-24 20:40:50 -0800

    refactor: Changes font size for sm and md screens.

--- components/Header.components.tsx
+++ components/Header.components.tsx
@@ -3,20 +3,21 @@ import { ThemeButton } from './ThemeButton.components';
 import { FiLogIn } from 'react-icons/fi';
 
 const playfair = Playfair_Display({
+  subsets: ['latin'],
   weight: '700',
 });
 
 export const Header = () => {
   return (
     <>
-      <div className="min-w-screen mx-auto md:max-w-[768px] flex justify-between mt-5 mb-10">
-        <button className="text-3xl">
-          <h1 className={playfair.className}>Stocksaver</h1>
+      <div className="min-w-screen mx-auto px-5 md:max-w-[768px] flex justify-between mt-5 mb-10">
+        <button className={playfair.className}>
+          <h1 className="text-2xl md:text-3xl">Tickersaver</h1>
         </button>
-        <div className="flex items-center gap-20">
+        <div className="flex items-center gap-10">
           <ThemeButton />
           <button>
-            <FiLogIn size={33} />
+            <FiLogIn size={27} />
           </button>
         </div>
       </div>
