ecc97b20ba74b7e243363022b0ef408ba5ae1261
Peter Yoo <trustintofu@gmail.com>
2022-11-24 21:22:38 -0800

    feat: Styles for dark theme, adds animated border for dark theme.

--- components/Signup.components.tsx
+++ components/Signup.components.tsx
@@ -46,79 +46,81 @@ export const Signup = () => {
     <>
       <form>
         <div className="md:mx-10">
-          <div className="max-w-[327px] md:max-w-[941px] mx-auto bg-white pt-4 md:pt-0 rounded-3xl shadow-md shadow-gray-500 md:grid md:grid-cols-2">
-            <div className="mt-4">
-              <h1 className="text-2xl font-bold text-center md:text-3xl">
-                Sign-Up!
-              </h1>
-              <div className="px-10">
-                <label>
-                  <h3 className="mt-5">Username:</h3>
-                  <div className="border border-gray-400 text-gray-400 hover:border-black hover:text-black flex items-center pl-2 rounded-md py-2">
-                    <BiUser />
-                    <input
-                      type="text"
-                      className="w-full px-2 focus:outline-none text-black"
-                    />
-                  </div>
-                </label>
-                <label>
-                  <h3 className="mt-2">Email:</h3>
-                  <div className="border border-gray-400 text-gray-400 hover:border-black hover:text-black flex items-center pl-2 rounded-md py-2">
-                    <MdAlternateEmail />
-                    <input
-                      type="email"
-                      className="w-full px-2 focus:outline-none text-black"
-                    />
-                  </div>
-                </label>
-                <label>
-                  <h3 className="mt-2">Password:</h3>
-                  <div className="border border-gray-400 text-gray-400 hover:border-black hover:text-black flex items-center pl-2 rounded-md py-2">
-                    <RiLockPasswordLine />
-                    <input
-                      type="password"
-                      className="w-full px-2 focus:outline-none text-black"
-                    />
-                  </div>
-                </label>
-                <label>
-                  <h3 className="mt-2">Confirm Password:</h3>
-                  <div className="border border-gray-400 text-gray-400 hover:border-black hover:text-black flex items-center pl-2 rounded-md py-2">
-                    <RiLockPasswordLine />
-                    <input
-                      type="password"
-                      className="w-full px-2 focus:outline-none text-black"
-                    />
-                  </div>
-                </label>
-                <button
-                  type="submit"
-                  className="bg-blue-400 w-full mt-5 text-white font-bold text-xl py-2 rounded-3xl"
-                >
-                  Sign-Up
-                </button>
-                <p className="text-xs text-center mt-3">
-                  Already have an account? Login here.
-                </p>
+          <div className="max-w-[327px] md:max-w-[937px] animate-border rounded-3xl from-purple-500 via-teal-500 to-blue-500 bg-[length:400%_400%] p-0.5 dark:bg-black dark:bg-gradient-to-r mx-auto">
+            <div className="bg-white pt-4 md:pt-0 rounded-3xl shadow-md shadow-gray-500 md:grid md:grid-cols-2 dark:shadow-none dark:bg-[#0f121a]">
+              <div className="mt-4">
+                <h1 className="text-2xl font-bold text-center md:text-3xl">
+                  Sign-Up!
+                </h1>
+                <div className="px-10">
+                  <label>
+                    <h3 className="mt-5">Username:</h3>
+                    <div className="border border-gray-400 text-gray-400 hover:border-black hover:text-black flex items-center pl-2 rounded-md py-2 dark:border-gray-700 dark:text-gray-700 dark:hover:border-white dark:hover:text-white">
+                      <BiUser />
+                      <input
+                        type="text"
+                        className="w-full px-2 focus:outline-none text-black dark:bg-[#0f121a] dark:text-white"
+                      />
+                    </div>
+                  </label>
+                  <label>
+                    <h3 className="mt-2">Email:</h3>
+                    <div className="border border-gray-400 text-gray-400 hover:border-black hover:text-black flex items-center pl-2 rounded-md py-2 dark:border-gray-700 dark:text-gray-700 dark:hover:border-white dark:hover:text-white">
+                      <MdAlternateEmail />
+                      <input
+                        type="email"
+                        className="w-full px-2 focus:outline-none text-black dark:bg-[#0f121a] dark:text-white"
+                      />
+                    </div>
+                  </label>
+                  <label>
+                    <h3 className="mt-2">Password:</h3>
+                    <div className="border border-gray-400 text-gray-400 hover:border-black hover:text-black flex items-center pl-2 rounded-md py-2 dark:border-gray-700 dark:text-gray-700 dark:hover:border-white dark:hover:text-white">
+                      <RiLockPasswordLine />
+                      <input
+                        type="password"
+                        className="w-full px-2 focus:outline-none text-black dark:bg-[#0f121a] dark:text-white"
+                      />
+                    </div>
+                  </label>
+                  <label>
+                    <h3 className="mt-2">Confirm Password:</h3>
+                    <div className="border border-gray-400 text-gray-400 hover:border-black hover:text-black flex items-center pl-2 rounded-md py-2 dark:border-gray-700 dark:text-gray-700 dark:hover:border-white dark:hover:text-white">
+                      <RiLockPasswordLine />
+                      <input
+                        type="password"
+                        className="w-full px-2 focus:outline-none text-black dark:bg-[#0f121a] dark:text-white"
+                      />
+                    </div>
+                  </label>
+                  <button
+                    type="submit"
+                    className="bg-blue-600 w-full mt-10 text-white font-bold text-xl py-2 rounded-3xl dark:hover:bg-white dark:hover:text-[#0f121a] hover:bg-black"
+                  >
+                    Sign-Up
+                  </button>
+                  <p className="text-xs text-center mt-3">
+                    Already have an account? Login here.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="">
+              <div className="">
+                <Image
+                  src="https://i.imgur.com/MdS6y7x.png"
+                  width={466}
+                  height={539}
+                  alt="hero desktop"
+                  className="hidden md:block"
+                />
+              </div>
               <Image
-                src="https://i.imgur.com/MdS6y7x.png"
-                width={466}
-                height={539}
-                alt="hero desktop"
-                className="hidden md:block"
+                src="https://i.imgur.com/LDfAqQA.png"
+                width={327}
+                height={135}
+                alt="hero mobile"
+                className="mt-10 md:hidden"
               />
             </div>
-            <Image
-              src="https://i.imgur.com/LDfAqQA.png"
-              width={327}
-              height={135}
-              alt="hero mobile"
-              className="mt-10 md:hidden"
-            />
           </div>
         </div>
       </form>
