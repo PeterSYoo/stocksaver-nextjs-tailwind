4f508932186e39b2c4041c13dc18c0df9ef500dd
Peter Yoo <trustintofu@gmail.com>
2022-11-24 21:11:41 -0800

    feat: Adds props for border animation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        border: 'border 4s ease infinite',
+      },
+      keyframes: {
+        border: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
       colors: {
         light: '#E6EAF0',
       },
