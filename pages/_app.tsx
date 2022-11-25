77f09ac9dd12be1e4dde2bff4a22a3b3716b0c8c
Peter Yoo <trustintofu@gmail.com>
2022-11-24 20:41:29 -0800

    feat: Adds open sans as global font.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,24 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import Head from 'next/head';
+import { Open_Sans } from '@next/font/google';
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Stocksaver - Save your tickers!</title>
+        <title>Tickersaver - Save your tickers!</title>
       </Head>
       <ThemeProvider enableSystem={true} attribute="class">
+        <style jsx global>{`
+          html {
+            font-family: ${openSans.style.fontFamily};
+          }
+        `}</style>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
