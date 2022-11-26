dd4e243f0c75cfb9fc6d5131b43ca6bfa1c3ea19
Peter Yoo <trustintofu@gmail.com>
2022-11-25 22:14:57 -0800

    feat: Creates loader spinner.

---
+++ components/LoaderSpinner.components.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import { MoonLoader } from 'react-spinners';
+
+export const LoaderSpinner = () => {
+  const { systemTheme, theme } = useTheme();
+  const [hydrated, setHydrated] = useState(false);
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  if (!hydrated) return null;
+
+  if (currentTheme === 'dark') {
+    return (
+      <>
+        <div className="min-w-screen min-h-screen mt-20 flex justify-center">
+          <MoonLoader color="#919191" size="30px" />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="min-w-screen min-h-screen mt-20 flex justify-center">
+          <MoonLoader color="#545454" size="30px" />
+        </div>
+      </>
+    );
+  }
+};
