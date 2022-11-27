082f2dae822908cdef5c67cd3553a04023baa161
Peter Yoo <trustintofu@gmail.com>
2022-11-26 22:19:08 -0800

    feat: Type for getServerSideProps.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
 import { Signup } from '../components/Signup.components';
 
@@ -22,7 +23,7 @@ const Home = () => {
 
 export default Home;
 
-export const getServerSideProps = async (context: any) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
 
   if (session) {
