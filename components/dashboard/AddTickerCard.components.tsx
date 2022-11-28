8363467e0762ad379f058e81195d881e180d16f4
Peter Yoo <trustintofu@gmail.com>
2022-11-27 22:44:16 -0800

    feat: Adds hover animation to button.

--- components/dashboard/AddTickerCard.components.tsx
+++ components/dashboard/AddTickerCard.components.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 export const AddTickerCard = () => {
   return (
     <>
-      <div className="bg-[#F1F6FA] flex flex-col justify-center items-center h-36 rounded-3xl shadow-md shadow-gray-500 dark:shadow-none dark:bg-black">
-        <h1 className="text-3xl font-bold">+</h1>
-      </div>
+      <Link href="/search">
+        <div className="bg-[#F1F6FA] flex flex-col justify-center items-center h-36 rounded-3xl shadow-md shadow-gray-500 dark:shadow-none dark:bg-black dark:text-gray-800 dark:hover:text-white text-gray-500 hover:text-black">
+          <h1 className="text-3xl font-bold">+</h1>
+        </div>
+      </Link>
     </>
   );
 };
