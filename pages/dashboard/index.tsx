28b0854b61152214a2292067152f2f9c8b0af009
Peter Yoo <trustintofu@gmail.com>
2022-12-02 23:55:36 -0800

    feat: Removes unused states and queries, only passes tickers data fetched from tickers connected to user document from mongo.

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,86 +1,20 @@
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
 import Link from 'next/link';
-import { useQuery } from '@tanstack/react-query';
 import { Profile } from '../../components/dashboard/Profile.components';
 import { TickerCards } from '../../components/dashboard/TickerCards.components';
 import { WinnersLosers } from '../../components/dashboard/WinnersLosers.components';
 import { LoaderSpinner } from '../../components/LoaderSpinner.components';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { getUser } from '../../lib/usersHelper';
-import { useEffect, useState } from 'react';
-import { deleteTickers } from '../../lib/tickersController';
-
-interface Ticker {
-  tickers: string;
-}
+import { getUserTickers } from '../../lib/userTickersHelper';
 
 const DashboardPage = ({ session }: any) => {
-  const APP_URL: any = process.env.NEXT_PUBLIC_APP_URL;
-  const [result, setResult] = useState([]);
-  const [tickers] = useState<string[]>([]);
-  const [combinedTickers, setCombinedTickers] = useState('');
-  const [apiKey] = useState(`${process.env.NEXT_PUBLIC_API_KEY}`);
-
-  const getUserTickers = async (userId: string) => {
-    const response = await fetch(`${APP_URL}/api/find-tickers/${userId}`);
-    const json = await response.json();
-
-    if (json) {
-      json.map((ticker: Ticker) => tickers.push(ticker.tickers));
-      let string = tickers.join();
-      setCombinedTickers(string);
-      return json;
-    }
-
-    return {};
-  };
-
-  const {
-    data: userData,
-    isLoading: userIsLoading,
-    isError: userIsError,
-    error: userError,
-  } = useQuery(['user'], () => getUser(session.user.id));
-
-  const {
-    data: tickersData,
-    isLoading: tickersIsLoading,
-    isError: tickersIsError,
-    error: tickersError,
-  } = useQuery(['userTickers'], () => getUserTickers(session.user.id));
-
-  useEffect(() => {
-    const fetchFromApi = async () => {
-      try {
-        const response = await fetch(
-          `https://api.stockdata.org/v1/data/quote?symbols=${combinedTickers}&api_token=${apiKey}`
-        );
-        const json = await response.json();
-
-        if (json) {
-          setResult(json.data);
-        }
-
-        return {};
-      } catch (error) {
-        return error;
-      }
-    };
-
-    if (combinedTickers.length !== 0) {
-      if (result.length === 0) {
-        fetchFromApi();
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [combinedTickers]);
-
-  if (userIsLoading || tickersIsLoading) return <LoaderSpinner />;
-  if (userIsError) return <>{userError}</>;
-  if (tickersIsError) return <>{tickersError}</>;
-
-  // console.log(result, 'results');
-  console.log(tickersData, 'tickers');
+  const user = useQuery(['user'], () => getUser(session.user.id));
+  const userTickers = useQuery(['userTickers'], () =>
+    getUserTickers(session.user.id)
+  );
 
   return (
     <>
@@ -94,11 +28,11 @@ const DashboardPage = ({ session }: any) => {
               dashboard
             </Link>
           </div>
-          <Profile user={userData} />
+          <Profile user={user?.data} />
           <div className="md:hidden">
-            <WinnersLosers results={result} />
+            <WinnersLosers />
           </div>
-          <TickerCards results={result} />
+          <TickerCards tickers={userTickers.data} />
         </div>
       </div>
     </>
