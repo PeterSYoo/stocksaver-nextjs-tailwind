1e8fa0ed139726775a65105ac3b1eca44db10832
Peter Yoo <trustintofu@gmail.com>
2022-12-02 23:52:49 -0800

    feat: Removes unused props from ticker card, passes ticker name prop.

--- components/dashboard/TickerCards.components.tsx
+++ components/dashboard/TickerCards.components.tsx
@@ -1,25 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import { Fragment } from 'react';
+import { getCompany, getPrice } from '../../lib/dashboardHelper';
+import { LoaderSpinner } from '../LoaderSpinner.components';
 import { AddTickerCard } from './AddTickerCard.components';
 import { TickerCard } from './TickerCard.components';
 
-export const TickerCards = ({ results }: any) => {
-  return (
-    <>
-      <div className="bg-white shadow-md shadow-gray-500 rounded-3xl px-5 py-6 grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-x-8 md:px-8 dark:shadow-none dark:bg-dark items-start md:items-start">
-        {results?.map((result: any) => (
-          <Fragment key={result?.ticker}>
-            <TickerCard
-              ticker={result?.ticker}
-              name={result?.name}
-              price={result?.price}
-              prevClose={result?.previous_close_price}
-            />
-          </Fragment>
-        ))}
-        <button>
-          <AddTickerCard />
-        </button>
-      </div>
-    </>
-  );
+export const TickerCards = ({ tickers }: any) => {
+  if (tickers) {
+    return (
+      <>
+        <div className="bg-white shadow-md shadow-gray-500 rounded-3xl px-5 py-6 grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-x-8 md:px-8 dark:shadow-dark3xl dark:bg-dark items-start md:items-start">
+          {tickers?.map((ticker: any) => (
+            <Fragment key={ticker.tickers}>
+              <TickerCard ticker={ticker.tickers} />
+            </Fragment>
+          ))}
+          <button>
+            <AddTickerCard />
+          </button>
+        </div>
+      </>
+    );
+  } else {
+    return null;
+  }
 };
