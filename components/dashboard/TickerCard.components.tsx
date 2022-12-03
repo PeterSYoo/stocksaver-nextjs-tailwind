13a2cdf033d9a828cf63941ca2be6d862c5fe15b
Peter Yoo <trustintofu@gmail.com>
2022-12-02 23:51:55 -0800

    feat: Fetches data from api, set data to company and price stats to be consumed in the card.

--- components/dashboard/TickerCard.components.tsx
+++ components/dashboard/TickerCard.components.tsx
@@ -1,94 +1,58 @@
-import { TiDelete } from 'react-icons/ti';
-import { deleteTickers } from '../../lib/tickersController';
+import { useQuery } from '@tanstack/react-query';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { getCompany, getPrice } from '../../lib/dashboardHelper';
+import { LoaderSpinner } from '../LoaderSpinner.components';
 
-interface Result {
-  price: number;
-  prevClose: number;
-  ticker: string;
-  name: string;
-}
+export const TickerCard = ({ ticker }: any) => {
+  const [apiKey] = useState(process.env.NEXT_PUBLIC_API_KEY);
+  const [company, setCompany] = useState<any>({});
+  const [price, setPrice] = useState<any>({});
 
-interface DayChange {
-  price: number;
-  prevClose: number;
-}
+  useEffect(() => {
+    const getData = async (ticker: any) => {
+      try {
+        const responseCompany = await fetch(
+          `https://finnhub.io/api/v1/stock/profile2?symbol=${ticker}&token=${apiKey}`
+        );
+        const responsePrice = await fetch(
+          `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${apiKey}`
+        );
+        const jsonCompany = await responseCompany.json();
+        const jsonPrice = await responsePrice.json();
 
-export const TickerCard = ({ ticker, name, price, prevClose }: Result) => {
-  const perIncrease = (a: number, b: number) => {
-    let percent;
-    if (b !== 0) {
-      if (a !== 0) {
-        percent = ((b - a) / a) * 100;
-      } else {
-        percent = b * 100;
+        if (jsonPrice && jsonCompany) {
+          console.log(jsonPrice);
+          console.log(jsonCompany);
+          setCompany(jsonCompany);
+          setPrice(jsonPrice);
+          // console.log(resultCompany, 'company');
+          // console.log(resultPrice, 'price');
+        }
+
+        return {};
+      } catch (error) {
+        return error;
       }
-    } else {
-      percent = -a * 100;
-    }
-    return percent.toFixed(3);
-  };
+    };
 
-  const percChange = ({ price, prevClose }: DayChange) => {
-    let pos;
-    let neg;
-
-    if (prevClose > price) {
-      neg = perIncrease(price, prevClose);
-      return (
-        <span className="text-xs font-bold bg-red-300 text-red-600 px-2 py-1 rounded-full">
-          -%{neg}
-        </span>
-      );
-    } else if (price > prevClose) {
-      pos = perIncrease(prevClose, price);
-      return (
-        <span className="text-xs font-bold bg-green-300 text-green-800 px-2 py-1 rounded-full">
-          +%{pos}
-        </span>
-      );
-    }
-  };
-
-  const dayChange = ({ price, prevClose }: DayChange) => {
-    let pos = price - prevClose;
-    let neg = prevClose - price;
-    let posString = pos.toString().substring(0, 7);
-    let negString = neg.toString().substring(0, 7);
-
-    if (prevClose > price) {
-      return <span className="text-red-500">{`-${negString}`}</span>;
-    } else if (price > prevClose) {
-      return <span className="text-green-600">{`+${posString}`}</span>;
-    }
-  };
+    getData(ticker);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ticker]);
 
   return (
     <>
-      <div className="flex flex-col gap-3">
-        <div className="bg-[#F1F6FA] flex flex-col justify-between rounded-3xl shadow-md shadow-gray-500 px-3 py-4 h-36 dark:shadow-none dark:bg-black">
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <span className="font-bold text-sm">{ticker}</span>
-              <span className="text-xs">{name}</span>
-            </div>
-            <div className="text-sm font-bold">${price}</div>
-          </div>
-          <div className="flex justify-between">
-            <div className="flex items-end text-xs">Daily</div>
-            <div className="flex flex-col items-center gap-1">
-              <span className="text-xs font-bold text-green-800">
-                {dayChange({ price, prevClose })}
-              </span>
-              <div>{percChange({ price, prevClose })}</div>
+      {Object.keys(company).length !== 0 ? (
+        <>
+          <div className="bg-white shadow-md shadow-gray-500 rounded-3xl px-8 md:px-14 pt-10 pb-14 dark:shadow-dark3xl dark:bg-dark flex flex-col gap-3 ">
+            <div className="bg-gray-200 rounded-3xl shadow-md shadow-gray-500 dark:bg-black dark:shadow-dark3xl">
+              {company?.name}
             </div>
           </div>
-        </div>
-        <div className="flex justify-center">
-          <button className="text-2xl text-red-600 hover:text-black dark:hover:text-white dark:bg-black dark:aspect-square p-[2px] rounded-md bg-[#F1F6FA] shadow shadow-gray-500 dark:shadow-none">
-            <TiDelete />
-          </button>
-        </div>
-      </div>
+        </>
+      ) : (
+        <LoaderSpinner />
+      )}
     </>
   );
 };
