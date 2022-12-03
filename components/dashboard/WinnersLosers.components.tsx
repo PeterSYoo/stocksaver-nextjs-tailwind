887f29d85136205245407c253b196e50a1273678
Peter Yoo <trustintofu@gmail.com>
2022-12-02 23:52:56 -0800

    feat: Removes unused prop.

--- components/dashboard/WinnersLosers.components.tsx
+++ components/dashboard/WinnersLosers.components.tsx
@@ -1,7 +1,7 @@
 import { LosersCard } from './LosersCard.components';
 import { WinnersCard } from './WinnersCard.components';
 
-export const WinnersLosers = ({ results }: any) => {
+export const WinnersLosers = () => {
   return (
     <>
       <div className="bg-white shadow-md shadow-gray-500 rounded-3xl px-4 py-4 flex flex-col gap-5 dark:shadow-none dark:bg-dark">
