1ae4688830cafdf0e25cf4cf99cd671505a45032
Peter Yoo <trustintofu@gmail.com>
2022-12-03 18:58:38 -0800

    feat: Styles add ticker card with hover animations.

--- components/dashboard/AddTickerCard.components.tsx
+++ components/dashboard/AddTickerCard.components.tsx
@@ -4,7 +4,7 @@ export const AddTickerCard = () => {
   return (
     <>
       <Link href="/search">
-        <div className="bg-[#F1F6FA] flex flex-col justify-center items-center h-36 rounded-3xl shadow-md shadow-gray-500 dark:shadow-none dark:bg-black dark:text-gray-800 dark:hover:text-white text-gray-500 hover:text-black">
+        <div className="bg-gray-200 flex flex-col justify-center items-center w-60 h-12 rounded-3xl shadow-md shadow-gray-500 dark:bg-black dark:text-white dark:hover:text-white text-black hover:bg-blue-700 duration-300 ease-in-out hover:text-white dark:hover:bg-blue-700 dark:shadow-dark3xl md:h-32 md:w-32 md:hover:h-40 md:hover:w-40 hover:w-72">
           <h1 className="text-3xl font-bold">+</h1>
         </div>
       </Link>
