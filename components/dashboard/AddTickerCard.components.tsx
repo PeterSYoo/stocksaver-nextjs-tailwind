899efce3d31a368d477a06b6665bc72d966f9741
Peter Yoo <trustintofu@gmail.com>
2022-12-03 19:38:34 -0800

    refactor: Changes width and height for both desktop and mobile.

--- components/dashboard/AddTickerCard.components.tsx
+++ components/dashboard/AddTickerCard.components.tsx
@@ -4,8 +4,10 @@ export const AddTickerCard = () => {
   return (
     <>
       <Link href="/search">
-        <div className="bg-gray-200 flex flex-col justify-center items-center w-60 h-12 rounded-3xl shadow-md shadow-gray-500 dark:bg-black dark:text-white dark:hover:text-white text-black hover:bg-blue-700 duration-300 ease-in-out hover:text-white dark:hover:bg-blue-700 dark:shadow-dark3xl md:h-32 md:w-32 md:hover:h-40 md:hover:w-40 hover:w-72">
-          <h1 className="text-3xl font-bold">+</h1>
+        <div className="bg-gray-200 rounded-3xl shadow-md shadow-gray-500 dark:bg-black dark:shadow-dark3xl flex flex-col gap-10 md:gap-0 md:h-72 justify-between w-72 h-12 md:w-64 hover:bg-blue-600 dark:hover:bg-gray-200 dark:hover:text-black hover:text-white">
+          <div className="flex flex-col justify-center items-center h-full text-3xl">
+            +
+          </div>
         </div>
       </Link>
     </>
