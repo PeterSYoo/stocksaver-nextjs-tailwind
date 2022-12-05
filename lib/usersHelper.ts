ce0a0c11d0c8ae84ba12e137cc569ce6525ac1e5
Peter Yoo <trustintofu@gmail.com>
2022-12-04 17:34:27 -0800

    feat: Adds get all users fetching function.

--- lib/usersHelper.ts
+++ lib/usersHelper.ts
@@ -1,14 +1,32 @@
 const APP_URL: any = process.env.NEXT_PUBLIC_APP_URL;
 
-export const getUser = async (userId: string) => {
-  const response = await fetch(`${APP_URL}/api/users/${userId}`);
-  const json = await response.json();
+// GET all users
+export const getUsers = async () => {
+  try {
+    const response = await fetch(`${APP_URL}/api/users`);
+    const json = await response.json();
 
-  if (json) return json;
-
-  return {};
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
+// GET single user
+export const getUser = async (userId: string) => {
+  try {
+    const response = await fetch(`${APP_URL}/api/users/${userId}`);
+    const json = await response.json();
+
+    if (json) return json;
+
+    return {};
+  } catch (error) {
+    return error;
+  }
+};
+
+// Update single user
 export const updateUser = async (userId: string, formData: string) => {
   try {
     const Options = {
