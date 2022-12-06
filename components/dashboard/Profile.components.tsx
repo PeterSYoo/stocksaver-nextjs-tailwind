792835c29065cd2a1875b6e34dcb9313e8410504
Peter Yoo <trustintofu@gmail.com>
2022-12-05 21:57:26 -0800

    style: changes text color.

--- components/dashboard/Profile.components.tsx
+++ components/dashboard/Profile.components.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { Playfair_Display } from '@next/font/google';
 import { WinnersCard } from './WinnersCard.components';
 import { LosersCard } from './LosersCard.components';
@@ -21,7 +20,7 @@ export const Profile = ({ user, winner, loser, tickers }: any) => {
               </span>
             </h1>
             <h1 className="text-md hidden md:block">
-              Tickers:{' '}
+              Tickers:&nbsp;
               <span className="dark:text-red-500 text-red-600 font-bold">
                 {tickers?.length}
               </span>
@@ -30,10 +29,17 @@ export const Profile = ({ user, winner, loser, tickers }: any) => {
         </div>
         <div className="flex flex-col justify-center items-center md:col-start-3 md:col-span-10">
           <h1 className="text-2xl font-bold mb-1 md:hidden">
-            <span className={playfair.className}>@{user?.username}</span>
+            <span className={playfair.className}>
+              &nbsp;
+              <span className="text-blue-700 dark:text-blue-400">@</span>
+              {user?.username}
+            </span>
           </h1>
           <h1 className="text-md md:hidden">
-            Tickers: <span className="font-normal">{tickers?.length}</span>
+            Tickers:&nbsp;
+            <span className="dark:text-red-500 text-red-600 font-bold">
+              {tickers?.length}
+            </span>
           </h1>
           <div className="md:flex gap-10 hidden ml-12 justify-center">
             <WinnersCard winner={winner} />
