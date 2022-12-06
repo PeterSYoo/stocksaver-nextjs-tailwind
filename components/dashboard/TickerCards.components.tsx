e6a36338bf860881eaf5d0c34076b77fe68dc5e4
Peter Yoo <trustintofu@gmail.com>
2022-12-05 20:07:38 -0800

    feat: Passes props.

--- components/dashboard/TickerCards.components.tsx
+++ components/dashboard/TickerCards.components.tsx
@@ -1,21 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
 import { Fragment } from 'react';
-import { getCompany, getPrice } from '../../lib/dashboardHelper';
-import { LoaderSpinner } from '../LoaderSpinner.components';
 import { AddTickerCard } from './AddTickerCard.components';
 import { TickerCard } from './TickerCard.components';
 
-export const TickerCards = ({ tickers, refetch }: any) => {
+export const TickerCards = ({
+  tickers,
+  refetch,
+  localTickers,
+  setLocalTickers,
+  setDeletedTicker,
+}: any) => {
   if (tickers) {
     return (
       <>
         <div className="bg-white shadow-md shadow-gray-500 rounded-3xl px-5 py-6 grid md:grid-cols-3 gap-8 md:gap-x-8 md:px-8 dark:shadow-dark3xl dark:bg-dark items-start md:items-start pb-20 md:pb-10">
-          {tickers?.map((ticker: any) => (
+          {localTickers?.map((ticker: any) => (
             <Fragment key={ticker._id}>
               <TickerCard
                 ticker={ticker.tickers}
                 refetch={refetch}
                 id={ticker._id}
+                localTickers={localTickers}
+                setLocalTickers={setLocalTickers}
+                setDeletedTicker={setDeletedTicker}
               />
             </Fragment>
           ))}
