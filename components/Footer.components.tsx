cc6871e8ba591193c88a48b2cb3f7f96b0ef8bad
Peter Yoo <trustintofu@gmail.com>
2022-12-06 23:36:51 -0800

    feat: Create footer component.

---
+++ components/Footer.components.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <>
+      <footer className="flex justify-center w-full my-20">
+        <p className="text-sm">Coded and Designed by Peter Yoo. &#169; 2022</p>
+      </footer>
+    </>
+  );
+};
