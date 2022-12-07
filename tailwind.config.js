f6dae4151359d675e0b8b8eadbcf3cb20720df54
Peter Yoo <trustintofu@gmail.com>
2022-12-06 23:36:13 -0800

    feat: Adds custom fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,8 +21,13 @@ module.exports = {
         dark: '#1e2124',
       },
       boxShadow: {
+        darkmd: '1px 1px 0px 0px rgba(87, 86, 83)',
         dark3xl: '3px 3px 0px 0px rgba(87, 86, 83)',
         darkHeader: '0px 3px 0px 0px rgba(87, 86, 83)',
+      },
+      fontFamily: {
+        sans: ['var(--font-openSans)'],
+        serif: ['var(--font-playfair)'],
       },
     },
     screens: {
